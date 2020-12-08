a8d89ddfff6446959966ce23d0d5a4e3f03b43a3
Minh Nguyen-Hue <hue.nguyen@employmenthero.com>
2020-12-08 17:21:41 +0700

    Add binding to comint

--- doom/+bindings.el
+++ doom/+bindings.el
@@ -673,6 +673,17 @@
 (map!
   :inv "C-<f6>" #'projectile-toggle-between-implementation-and-test
   :nv "s-'" #'+vterm/toggle
+  (:map comint-mode-map
+    :i "s-1"   #'+workspace/switch-to-0
+    :i "s-2"   #'+workspace/switch-to-1
+    :i "s-3"   #'+workspace/switch-to-2
+    :i "s-4"   #'+workspace/switch-to-3
+    :i "s-5"   #'+workspace/switch-to-4
+    :i "s-6"   #'+workspace/switch-to-5
+    :i "s-7"   #'+workspace/switch-to-6
+    :i "s-8"   #'+workspace/switch-to-7
+    :i "s-9"   #'+workspace/switch-to-8
+    :i "s-0"   #'+workspace/switch-to-final)
   (:after vterm :map vterm-mode-map
     :i "s-1"   #'+workspace/switch-to-0
     :i "s-2"   #'+workspace/switch-to-1
