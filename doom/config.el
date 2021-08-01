9b20f967dd9b9aee22e1be9f67d4545a0bf3f948
suonlight <minh.nh1989@gmail.com>
2021-08-01 22:36:08 +0700

    Use FiraCode for doom

--- doom/config.el
+++ doom/config.el
@@ -39,7 +39,8 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 14))
-(setq doom-font (font-spec :family "Source Code Pro" :size 14))
+;; (setq doom-font (font-spec :family "Source Code Pro" :size 14))
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
