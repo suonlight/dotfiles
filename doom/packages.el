5d0deb6d758181a00c1cd02b748be4426d683c72
suonlight <minh.nh1989@gmail.com>
2021-10-09 08:23:30 +0700

    Try nano theme

--- doom/packages.el
+++ doom/packages.el
@@ -116,8 +116,6 @@
 (package! evil-string-inflection)
 ;; (package! counsel-etags)
 
-;; (package! git-link) ;; use browse-at-remote instead
-
 (package! ob-tmux)
 ;; (package! ob-mermaid)
 
@@ -126,6 +124,8 @@
 (package! read-aloud)
 (package! websocket)
 (package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
+(package! nano-theme :recipe (:host github :repo "rougier/nano-theme"))
+(package! nano-modeline :recipe (:host github :repo "rougier/nano-modeline"))
 
 (when IS-LINUX (package! exwm))
 
