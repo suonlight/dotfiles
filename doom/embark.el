2c9a0d10566f6be8ef536cd321eb953212f28a38
suonlight <minh.nh1989@gmail.com>
2022-06-21 18:02:42 +0700

    Add C-v to vsplit when bookmark-jump

--- doom/embark.el
+++ doom/embark.el
@@ -21,6 +21,7 @@
 (after! embark
   (define-key embark-buffer-map   (kbd "C-v") (embark-vsplit switch-to-buffer))
   (define-key embark-file-map     (kbd "C-v") (embark-vsplit projectile-find-file))
+  (define-key embark-bookmark-map (kbd "C-v") (embark-vsplit bookmark-jump))
 
   (define-key embark-buffer-map   (kbd "C-s") (embark-split switch-to-buffer))
   (define-key embark-file-map     (kbd "C-s") (embark-split projectile-find-file)))
