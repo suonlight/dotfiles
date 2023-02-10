ad346519635b0c51e9ce7939413ff0c650ece553
suonlight <minh.nh1989@gmail.com>
2023-02-10 15:11:35 +0700

    Add ob-chatgpt with web-based and api-based

--- doom/modules/suonlight/chatgpt/config.el
+++ doom/modules/suonlight/chatgpt/config.el
@@ -1,9 +1,7 @@
 (use-package! chatgpt
-  :init
-  (require 'python)
-  (setq chatgpt-repo-path "~/.config/emacs/.local/straight/repos/ChatGPT.el/")
   :bind ("C-c q" . chatgpt-query)
   :config
+
   (setq chatgpt-query-format-string-map
     '(("doc" . "Please write the documentation for the following function.\n\n%s")
        ("bug" . "There is a bug in the following function, please help me fix it.\n\n%s")
@@ -11,3 +9,11 @@
        ("improve" . "Please improve the following code.\n\n%s")
        ;; your new prompt
        ("question" . "%s"))))
+
+(load! "chatgpt-api")
+
+(after! org
+  ;; (prefer-coding-system 'utf-8)
+  (load! "ob-chatgpt")
+
+  (add-to-list 'org-src-lang-modes '("chatgpt" . fundamental)))
