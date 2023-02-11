3ea85728e17b4555384ceab59c95f7b254d433ed
suonlight <minh.nh1989@gmail.com>
2023-02-11 19:54:11 +0700

    [emacs] Move ob-chatgpt into github repo

--- doom/modules/suonlight/chatgpt/config.el
+++ doom/modules/suonlight/chatgpt/config.el
@@ -1,18 +1,20 @@
 (use-package! chatgpt
+  :defer t
   :bind ("C-c q" . chatgpt-query)
   :config
-
+  (setq chatgpt-repo-path "~/.config/emacs/.local/straight/repos/ChatGPT.el/")
   (setq chatgpt-query-format-string-map
     '(("doc" . "Please write the documentation for the following function.\n\n%s")
        ("bug" . "There is a bug in the following function, please help me fix it.\n\n%s")
        ("understand" . "What does the following function do?\n\n%s")
        ("improve" . "Please improve the following code.\n\n%s")
        ;; your new prompt
-       ("question" . "%s"))))
+       ("question" . "%s")
+       ("translate" . "Translate to Vietnamese\n\n%s")
+       ("correct grammar" . "Correct grammar for the following text.\n\n%s")
+       ("paraphase" . "Write it in another way.\n\n%s")))
+  (set-popup-rule! (regexp-quote "*ChatGPT*")
+    :side 'bottom :size .5 :ttl nil :quit t :modeline nil))
 
-(load! "chatgpt-api")
-
-(after! org
-  (load! "ob-chatgpt")
-
-  (add-to-list 'org-src-lang-modes '("chatgpt" . fundamental)))
+(use-package! ob-chatgpt
+  :after '(org chatgpt))
--- doom/modules/suonlight/chatgpt/packages.el
+++ doom/modules/suonlight/chatgpt/packages.el
@@ -1,1 +1,2 @@
 (package! chatgpt :recipe (:host github :repo "joshcho/ChatGPT.el" :files ("dist" "*.el")))
+(package! ob-chatgpt :recipe (:host github :repo "suonlight/ob-chatgpt" :files ("dist" "*.el")))
