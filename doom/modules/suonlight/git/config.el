5270e05261d3f105f82bbabdd18080517ccaa093
suonlight <minh.nh1989@gmail.com>
2023-04-11 08:47:19 +0700

    [emacs] add bindings for magit-circleci

--- doom/modules/suonlight/git/config.el
+++ doom/modules/suonlight/git/config.el
@@ -1,6 +1,11 @@
+(after! magit
+  (map! (:map magit-mode-map
+          :nv "gcp" #'magit-circleci-pull
+          :nv "gcb" #'magit-circleci-browse-build
+          :nv "gct" #'magit-circleci-mode)))
+
 (use-package! magit-circleci
-  ;; :after 'magit
+  :commands (magit-circleci-pull magit-circleci-browse-build magit-circleci-mode)
   :config
-  ;; (setq magit-circleci-organisation-name (getenv "MAGIT_CIRCLECI_ORGANISATION_NAME"))
   (setq magit-circleci-token (getenv "MAGIT_CIRCLECI_TOKEN"))
   (setq magit-circleci--project-slug  (getenv "MAGIT_CIRCLECI_ORGANISATION_NAME")))
