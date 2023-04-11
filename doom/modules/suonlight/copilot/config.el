16536f7105fa0cb052fc0e2b8e78fdd18326f3f0
suonlight <minh.nh1989@gmail.com>
2023-04-11 08:48:40 +0700

    [emacs] refactor config copilot

--- doom/modules/suonlight/copilot/config.el
+++ doom/modules/suonlight/copilot/config.el
@@ -1,11 +1,7 @@
-;; Enable copilot-mode automatically
-(add-hook 'prog-mode-hook 'copilot-mode)
-
-;; accept completion from copilot and fallback to company
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
-  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
+  :bind (:map copilot-completion-map
+         ("C-TAB" . 'copilot-accept-completion-by-word)
          ("C-<tab>" . 'copilot-accept-completion-by-word)
-         :map copilot-completion-map
          ("<tab>" . 'copilot-accept-completion)
          ("TAB" . 'copilot-accept-completion)))
