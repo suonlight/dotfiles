722b934dbaf0783cf8af4ef44f86cd9a2698bb56
suonlight <minh.nh1989@gmail.com>
2023-04-13 16:37:01 +0700

    [emacs] add keybind for approve workflow in circleci

--- doom/modules/suonlight/git/config.el
+++ doom/modules/suonlight/git/config.el
@@ -1,11 +1,12 @@
 (after! magit
   (map! (:map magit-mode-map
+          :nv "gca" #'magit-circleci--approve-workflow
           :nv "gcp" #'magit-circleci-pull
           :nv "gcb" #'magit-circleci-browse-build
           :nv "gct" #'magit-circleci-mode)))
 
 (use-package! magit-circleci
-  :commands (magit-circleci-pull magit-circleci-browse-build magit-circleci-mode)
+  :commands (magit-circleci-pull magit-circleci-browse-build magit-circleci-mode magit-circleci--approve-workflow)
   :config
   (setq magit-circleci-token (getenv "MAGIT_CIRCLECI_TOKEN"))
   (setq magit-circleci--project-slug  (getenv "MAGIT_CIRCLECI_ORGANISATION_NAME")))
