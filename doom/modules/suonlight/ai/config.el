67f4594084af3da3135e6624c11e8ff6491b9c7c
suonlight <minh.nh1989@gmail.com>
2023-07-29 11:17:48 +0700

    Add ai bard & bing chat

--- doom/modules/suonlight/ai/config.el
+++ doom/modules/suonlight/ai/config.el
@@ -1,3 +1,10 @@
 (use-package! bard
   :defer t
-  :commands (bard-chat bard-chat-with-multiline))
+  :commands (bard-chat bard-chat-with-multiline)
+  :config
+  (setq bard-http-proxy ""))
+
+(use-package! aichat
+  :config
+  ;; (setq aichat-bingai-cookies-file (format "%s/.config/bing.cookies.json" (getenv "HOME")))
+  (setq aichat-http-backend 'url))
--- doom/modules/suonlight/ai/packages.el
+++ doom/modules/suonlight/ai/packages.el
@@ -1,2 +1,10 @@
 (package! bard
-  :recipe (:host github :repo "AllTheLife/Bard.el" :files ("*.el" "dist" "*.py")))
+  ;; :recipe (:host github :repo "AllTheLife/Bard.el" :files ("*.el" "dist" "*.py")))
+  :recipe (:host github :repo "EdmondFrank/Bard.el" :files ("*.el" "dist" "*.py")))
+
+(package! aichat
+  :recipe (:host github :repo "xhcoding/emacs-aichat" :files ("*.el" "dist" "*.py")))
+
+(package! async-await
+  :recipe (:host github :repo "chuntaro/emacs-async-await"))
+
