0cce56125535b454b47a2a4061943ca9cbc9d83d
suonlight <minh.nh1989@gmail.com>
2023-08-13 08:12:51 +0700

    [emacs] add bindings to use ai chat: bard and bing ai

--- doom/modules/suonlight/ai/config.el
+++ doom/modules/suonlight/ai/config.el
@@ -1,6 +1,6 @@
 (use-package! bard
   :defer t
-  :commands (bard-chat bard-chat-with-multiline)
+  :commands (bard-chat bard-chat-with-multiline bard-chat-with-message)
   :config
 
   ;; override bard-response to insert the response with org format in the current buffer
@@ -23,7 +23,44 @@
   (map! :leader "aa" #'bard-chat))
 
 (use-package! aichat
+  :commands (aichat-read-region-or-input aichat-bingai-conversation aichat-bingai-chat)
   :config
   ;; (setq aichat-bingai-cookies-file (format "%s/.config/bing.cookies.json" (getenv "HOME")))
   (setq aichat-http-backend 'url)
   (map! :leader "ai" #'aichat-bingai-chat))
+
+(defun bing-chat-with-message (text &optional style)
+  "Send the region or input to Bing and replace the selected region or insert at the current position with the returned result."
+  (interactive (list (aichat-read-region-or-input "Input text: ")))
+  (when (and text (not (string-empty-p text)))
+    (let* ((cur-buf (current-buffer))
+           (cur-pos (with-current-buffer cur-buf (point)))
+           (reg-beg (when (use-region-p) (region-beginning)))
+           (reg-end (when (use-region-p) (region-end))))
+      (aichat-bingai-conversation text
+                                  :style style
+                                  :on-success (lambda (msg)
+                                                (when-let ((content (aichat-bingai-message-type-2-text msg)))
+                                                  (save-excursion
+                                                    (goto-char (point-max))
+                                                    (insert "\n## User:\n")
+                                                    (insert (cdr (assq 'text (aichat-json-access msg "{item}{messages}[0]"))))
+                                                    (insert "\n\n## Bing AI:\n")
+                                                    (insert (format "%s\n" content)))
+                                                  (message "Success: %s" content)))
+                                  :on-error (lambda (err)
+                                              (message "Error: %s" err))))))
+
+;; write a method to firstly choose AI prompts: fix grammar, explain code, custom prompt
+;; then ask to choose which AI: bard-chat or aichat-bingai-chat to use
+;; then passing the chosen prompt to chose AI to chat
+(defun ai-chat ()
+  (interactive)
+  (let* ((prompt (read-string "Prompt: "))
+         (ai (completing-read "AI: " '("Google Bard" "Bing AI"))))
+    (cond ((equal ai "Google Bard")
+           (bard-chat-with-message prompt))
+          ((equal ai "Bing AI")
+           (bing-chat-with-message prompt)))))
+
+(map! :leader "aq" #'ai-chat)
