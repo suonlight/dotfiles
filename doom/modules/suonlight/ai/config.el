3e093b680ad05c1667a935e422a1e6ce39d5caca
suonlight <minh.nh1989@gmail.com>
2023-08-14 18:55:11 +0700

    [emacs] Optimise ai chat with region

--- doom/modules/suonlight/ai/config.el
+++ doom/modules/suonlight/ai/config.el
@@ -1,7 +1,22 @@
 (use-package! bard
-  :defer t
   :commands (bard-chat bard-chat-with-multiline bard-chat-with-message)
   :config
+  (defun bard-chat-with-message (prompt)
+    (message "[Bard] Please wait for Bard...")
+    (bard-call-async "bard_chat"
+      prompt
+      (buffer-name)))
+
+  (defun bard-chat ()
+    (interactive)
+    (let ((prompt (read-string "Chat with Bard: ")))
+      (if (string-empty-p (string-trim prompt))
+        (message "Please do not enter an empty prompt.")
+        (save-excursion
+          (goto-char (point-max))
+          (insert "## User:\n")
+          (insert (format "%s\n" prompt)))
+        (bard-chat-with-message prompt))))
 
   ;; override bard-response to insert the response with org format in the current buffer
   (defun bard-response (serial-number content buffer)
@@ -29,38 +44,46 @@
   (setq aichat-http-backend 'url)
   (map! :leader "ai" #'aichat-bingai-chat))
 
-(defun bing-chat-with-message (text &optional style)
+(defun bing-chat-with-region (text &optional selected)
   "Send the region or input to Bing and replace the selected region or insert at the current position with the returned result."
-  (interactive (list (aichat-read-region-or-input "Input text: ")))
-  (when (and text (not (string-empty-p text)))
-    (let* ((cur-buf (current-buffer))
-           (cur-pos (with-current-buffer cur-buf (point)))
-           (reg-beg (when (use-region-p) (region-beginning)))
-           (reg-end (when (use-region-p) (region-end))))
-      (aichat-bingai-conversation text
-                                  :style style
-                                  :on-success (lambda (msg)
-                                                (when-let ((content (aichat-bingai-message-type-2-text msg)))
-                                                  (save-excursion
-                                                    (goto-char (point-max))
-                                                    (insert "\n## User:\n")
-                                                    (insert (cdr (assq 'text (aichat-json-access msg "{item}{messages}[0]"))))
-                                                    (insert "\n\n## Bing AI:\n")
-                                                    (insert (format "%s\n" content)))
-                                                  (message "Success: %s" content)))
-                                  :on-error (lambda (err)
-                                              (message "Error: %s" err))))))
+  (interactive)
+  (save-excursion
+    (goto-char (point-max))
+    (when (not selected)
+      (insert "\n## User:\n")
+      (insert (format "%s\n\n\n" text))))
+  (message "[Bing AI] Please wait for Bing AI...")
+  (aichat-bingai-conversation text
+    :on-success (lambda (msg)
+                  (when-let ((content (aichat-bingai-message-type-2-text msg)))
+                    (save-excursion
+                      (goto-char (point-max))
+                      (insert "## Bing AI:\n")
+                      (insert (format "%s\n" content)))
+                    (message "Success: %s" content)))
+    :on-error (lambda (err) (message "Error: %s" err))))
+
+(defun bard-chat-with-region (prompt &optional selected)
+  (when (not selected)
+    (goto-char (point-max))
+    (save-excursion
+      (insert "## User:\n")
+      (insert (format "%s\n" prompt))))
+  (bard-chat-with-message prompt))
 
 ;; write a method to firstly choose AI prompts: fix grammar, explain code, custom prompt
-;; then ask to choose which AI: bard-chat or aichat-bingai-chat to use
-;; then passing the chosen prompt to chose AI to chat
 (defun ai-chat ()
   (interactive)
-  (let* ((prompt (read-string "Prompt: "))
-         (ai (completing-read "AI: " '("Google Bard" "Bing AI"))))
+  (let* ((selected (use-region-p))
+          (prompt (if selected
+                    (buffer-substring (region-beginning) (region-end))
+                    (read-string "Prompt: ")))
+          (ai (completing-read "AI: " '("Google Bard" "Bing AI"))))
+    (when selected
+      (deactivate-mark))
     (cond ((equal ai "Google Bard")
-           (bard-chat-with-message prompt))
-          ((equal ai "Bing AI")
-           (bing-chat-with-message prompt)))))
+            (bard-chat-with-region prompt selected))
+      ((equal ai "Bing AI")
+        (bing-chat-with-region prompt selected)))))
 
 (map! :leader "aq" #'ai-chat)
