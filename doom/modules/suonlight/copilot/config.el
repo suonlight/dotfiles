657eedb6e14ec76f156a02a681464479a227208c
suonlight <minh.nh1989@gmail.com>
2024-01-14 11:30:14 +0700

    [emacs] disable copilot warnings

--- doom/modules/suonlight/copilot/config.el
+++ doom/modules/suonlight/copilot/config.el
@@ -6,4 +6,6 @@
           ("C-TAB" . 'copilot-accept-completion-by-word)
           ("C-<tab>" . 'copilot-accept-completion-by-word)
           ("<tab>" . 'copilot-accept-completion)
-          ("TAB" . 'copilot-accept-completion)))
+          ("TAB" . 'copilot-accept-completion))
+  :config
+  (setq copilot-indent-offset-warning-disable t))
