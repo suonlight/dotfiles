25d8c3f1c0abf5ecce88d0bf0893a81e7af2ede3
suonlight <minh.nh1989@gmail.com>
2024-03-08 19:27:17 +0700

    [emacs] Auto new tmux session and window if not exist

--- doom/modules/suonlight/notes/astmux_server.py
+++ doom/modules/suonlight/notes/astmux_server.py
@@ -6,57 +6,110 @@ from epc.server import EPCServer
 
 server = EPCServer(('localhost', 0))
 
-@server.register_function
-def execute_astmux(session, lang, command):
+def wrap_command(jid, lang, command):
+    command = '; '.join(map(lambda x: x.strip(), command.split("\n")))
+    if lang == 'ruby':
+        return f'puts "# start:{jid}"; {command}; puts "# finish:{jid}"'
+    elif lang == 'sh':
+        return f'echo "# start:{jid}"; {command}; echo "# finish:{jid}"'
+    else:
+        return None
+
+def run_command_in_tmux(session, window, wrapped_command):
+    subprocess.run(['tmux', 'send-keys', '-t', f'{session}:{window}', wrapped_command, 'C-m'], check=True)
+
+def capture_tmux_output(session, jid):
+    output = ""
+    while True:
+        time.sleep(1)
+        capture_process = subprocess.run([
+            'tmux', 'capture-pane', '-J', '-p', '-S', '-', '-t', session
+        ], capture_output=True, text=True, check=False)
+        captured_output = capture_process.stdout
+        output += captured_output
+        finish_match = re.search(f"# finish:{jid}", output)
+        if finish_match and finish_match.start() != finish_match.end():
+            break
+
+    return extract_output(jid, output)
+
+def extract_output(jid, output):
+    start_match = re.search(f"\n# start:{jid}", output)
+    finish_match = re.search(f"\n# finish:{jid}", output)
+
+    if start_match and finish_match:
+        start_index = start_match.end()
+        finish_index = finish_match.start()
+        extracted_output = output[start_index:finish_index]
+
+        extracted_output = re.sub(f"# start:{uuid}", "", extracted_output)
+        extracted_output = re.sub(f"# finish:{uuid}", "", extracted_output)
+        extracted_output = extracted_output.strip()
+
+        return extracted_output
+    else:
+        return None
+
+def check_session_existence(session):
     try:
-        # Generate a unique UUID
-        unique_id = str(uuid.uuid4())
+        subprocess.run(['tmux', 'has-session', '-t', session], check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
-        # Modify the command to include the UUID
-        if lang == 'ruby':
-            wrapped_command = f'puts "# start:{unique_id}"; {command}; puts "# finish:{unique_id}"'
-        elif lang == 'sh':
-            wrapped_command = f'echo "# start:{unique_id}"; {command}; echo "# finish:{unique_id}"'
+def check_window_existence(session, window):
+    try:
+        subprocess.run(['tmux', 'list-windows', '-t', f'{session}:{window}'], check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def create_session(session):
+    subprocess.run(['tmux', 'new-session', '-d', '-s', session], check=True)
+
+def create_window(session, window):
+  subprocess.run([
+    'tmux', 'new-window',
+     "-c", "~",
+     '-n', window,
+     '-t', session
+  ], check=True)
+
+  # sleep to wait for window to be created
+  time.sleep(1)
+
+@server.register_function
+def execute_astmux(session, lang, jid, command):
+    try:
+        jid = str(jid)
+        wrapped_command = wrap_command(jid, lang, command)
+
+        if wrapped_command is None:
+            return {'jid': jid, 'status': 'error', 'message': 'Unsupported language'}
+
+        # Extract window from session (session_name:window_name or session_name)
+        session_parts = session.split(':')
+        if len(session_parts) > 1:
+            session_name, window_name = session_parts[0], session_parts[1]
         else:
-            return {'status': 'error', 'message': 'Unsupported language'}
+            session_name, window_name = session_parts[0], '0'
 
-        # Run the modified command using tmux
-        subprocess.run(['tmux', 'send-keys', '-t', session, wrapped_command, 'C-m'], check=True)
+        if not check_session_existence(session_name):
+            create_session(session_name)
 
-        # Periodically get tmux pane output using tmux capture-pane
-        output = ""
-        while True:
-            time.sleep(1)  # Adjust the sleep interval as needed
-            capture_process = subprocess.run([
-                'tmux', 'capture-pane', '-J', '-p', '-S', '-', '-t', session
-            ], capture_output=True, text=True, check=False)
-            captured_output = capture_process.stdout
+        if not check_window_existence(session_name, window_name):
+            create_window(session_name, window_name)
 
-            output += captured_output
-            finish_match = re.search(f"# finish:{unique_id}", output)
-            if finish_match and finish_match.start() != finish_match.end():
-                break
+        run_command_in_tmux(session_name, window_name, wrapped_command)
+        output = capture_tmux_output(session_name, jid)
 
-        original_output = output
-        start_match = re.search(f"# start:{unique_id}", output)
-        finish_match = re.search(f"# finish:{unique_id}", output)
-
-        if start_match and finish_match:
-            start_index = start_match.end()
-            output = output[start_index:]
-
-            print("---------------------")
-            print(output)
-
-            output = re.sub(f"# start:{uuid}", "", output)
-            output = re.sub(f"# finish:{uuid}", "", output)
-
-            return {'start': start_match.end(), 'output': output, 'aaa': original_output }
+        if output is not None:
+            return {'jid': jid, 'status': 'ok', 'message': output}
         else:
-            return {'start': start_match, 'finish': finish_match, 'output': output }
+            return {'jid': jid, 'status': 'error', 'message': output}
 
     except subprocess.CalledProcessError as e:
-        return {'status': 'error', 'message': str(e)}
+        return {'jid': jid, 'status': 'error', 'message': str(e)}
 
 server.print_port()
 server.serve_forever()
--- doom/modules/suonlight/notes/ob-astmux.el
+++ doom/modules/suonlight/notes/ob-astmux.el
@@ -1,17 +1,66 @@
-;; Define the ob-astmux language
+(require 'epc)
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((astmux . t)))
+
+(setq org-babel-default-header-args:astmux
+    '((:results . "none")
+       (:session . "default")
+       (:socket  . nil)))
 
 (defconst astmux-server-path
   (expand-file-name "astmux_server.py" (file-name-directory load-file-name))
   "Path to the astmux server script.")
 
+(defun astmux--generate-uuid ()
+  "Generate a 32 character UUID."
+  (md5 (number-to-string (random 100000000))))
+
+;; Customize language
+(defun astmux-get-lang (current-lang)
+  (let* ((info (org-babel-get-src-block-info))
+          (params (nth 2 info))
+          (custom-lang (cdr (assq :lang params))))
+
+    (cond ((string= current-lang "tmux")
+            custom-lang)
+      (t current-lang))))
+
+(add-to-list 'org-src-lang-modes '("tmux" . sh))
+
+(defun org-src---get-lang-mode (orig-fun &rest args)
+  (let* ((lang (astmux-get-lang (car args))))
+    (apply orig-fun (list lang))))
+
+(advice-add 'org-src-get-lang-mode :around #'org-src---get-lang-mode)
+
 (defun org-babel-execute:astmux (body params)
-  "Execute the astmux commands specified in BODY using EPC."
+  "Execute the astmux commands specified in BODY asynchronously using EPC."
   (let* ((session (cdr (assoc :session params)))
-          (lang (cdr (assoc :lang params)))
-          (connection (epc:start-epc "python3" `(,astmux-server-path)))
-          (result (epc:call-sync connection 'execute_astmux `(,session ,lang ,body))))
-    (epc:stop-epc connection)
-    result))
+         (lang (cdr (assoc :lang params)))
+         (results (or (cdr (assq :results params))))
+         (file (cdr (assq :file params)))
+         (jid (astmux--generate-uuid))
+         (connection (epc:start-epc "python3" `(,astmux-server-path))))
+    (deferred:$
+      (epc:call-deferred connection 'execute_astmux `(,session ,lang ,jid ,body))
+      (deferred:nextc it
+        `(lambda (response)
+           (with-current-buffer ,(current-buffer)
+             (save-excursion
+               (let* ((result (plist-get response :message))
+                       (file ,file))
+                 (goto-char (point-min))
+                 (search-forward ,jid)
+                 (search-backward "src")
+                 (if (not (eq file 'nil))
+                   (progn
+                     (write-region result nil file)
+                     (org-babel-insert-result file '("file" "replace")))
+                   (cond
+                     ((s-contains? "silent" ,results)
+                       (org-babel-remove-result)
+                       (message result))
+                     (t (org-babel-insert-result result '("replace")))))))))))
+    (org-babel-insert-result jid '("replace"))))
