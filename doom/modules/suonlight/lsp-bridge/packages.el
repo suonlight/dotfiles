65226deb9d2a629b2321fb22388f2090c729eadd
suonlight <minh.nh1989@gmail.com>
2024-03-25 11:10:09 +0700

    [emacs] pin lsp-bridge to a specific commit

--- doom/modules/suonlight/lsp-bridge/packages.el
+++ doom/modules/suonlight/lsp-bridge/packages.el
@@ -1,4 +1,6 @@
-(package! lsp-bridge :recipe (:host github :repo "manateelazycat/lsp-bridge" :files ("*")))
+(package! lsp-bridge
+          :pin "eaeb3bdda02f6e4cf153ed67c2598f9d37edd613"
+          :recipe (:host github :repo "manateelazycat/lsp-bridge" :files ("*")))
 ;; (package! lsp-bridge-diagnostics :recipe (:host github :repo "jadestrong/lsp-bridge-diagnostics" :files ("*")))
 ;; (package! lsp-mode :disable t :ignore t)
 (package! company :disable t :ignore t)
