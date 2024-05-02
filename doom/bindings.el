471c136f24bcd562b8f91690bd6f0da62dac9017
Minh Nguyen Hue <minhnguyenhue@Minhs-MacBook-Pro.local>
2024-05-02 17:58:29 +0700

    [emacs] temp to disable org-node keybindings

--- doom/bindings.el
+++ doom/bindings.el
@@ -699,7 +699,7 @@
       :nv "C-p"   #'projectile-find-file
       :nv "s-b"   #'projectile-switch-to-buffer
       :nv "<f10>" #'doom/window-maximize-buffer
-      :nv "<f9>"   #'org-node-find
+      :nv "<f9>"   #'org-roam-node-find
       :nv "<f12>" #'multi-vterm-project
       :nv "gh"    #'sl/jump-to-cursor
       :nv "s-`"   #'+evil/next-frame
@@ -818,7 +818,7 @@
     "C-j"          #'evil-window-down
     "C-k"          #'evil-window-up)
   (:after org :map org-mode-map
-    :i  "s-r"    #'org-node-insert-link
+    :i  "s-r"    #'org-roam-node-insert
     :nv "t"      #'org-todo)
   (:map xwidget-webkit-mode-map
     :nv "j"      #'xwidget-webkit-scroll-up ;; main object is scroller
